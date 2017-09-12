4e59bbe8e0af9c638acce289c0a477bd3e9cff8a
kkrao432 <31875710+kkrao432@users.noreply.github.com>
2017-09-12 13:33:50 +0530

    Add files via upload

---
+++ source/source/Main.java
@@ -0,0 +1,60 @@
+import java.lang.*;
+import java.util.Scanner;
+import java.util.*;
+import java.text.*;
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		/*int id;
+		String fullname;
+		String birthdate;
+		double avgmarks;
+		Date date1;
+		
+		
+		
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		Student s[]=new Student[n];
+		
+		
+		for(int i=0;i<n;i++)
+		{
+		id=sc.nextInt();
+		fullname=sc.next();
+		birthdate=sc.next();
+		avgmarks=sc.nextDouble();
+		date1=new SimpleDateFormat("dd/MM/yyyy").parse(birthdate);  
+		s[i]=new Student(id,fullname,date1,avgmarks);
+		
+		}
+		id=1424;
+		fullname="kondalarao";
+		birthdate="12/12/2012";
+		date1=new SimpleDateFormat("dd/MM/yyyy").parse(birthdate);
+		avgmarks=125;
+		s[1]=new Student(id,fullname,date1,avgmarks);
+		id=1425;
+		fullname="griet";
+		birthdate="12/12/2002";
+		date1=new SimpleDateFormat("dd/MM/yyyy").parse(birthdate);
+		avgmarks=625;
+		s[2]=new Student(id,fullname,date1,avgmarks);
+		StudentGroup sg=new StudentGroup(2);
+		for(int i=0;i<2;i++)
+		{
+			sg.getStudents();
+		}
+		
+		*/
+		
+		
+		
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
+
